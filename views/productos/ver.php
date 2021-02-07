a20ee4f5b468de4e60b29b2c9b8a8ac07f63ec07
W.uribe <wilsonuribe16@gmail.com>
2021-02-07 03:53:40 +0100

    css de productos destacados

--- views/productos/ver.php
+++ views/productos/ver.php
@@ -1,15 +1,19 @@
 <?php if (isset($produc)) : ?>
     <h1><?= $produc->nombre ?></h1>
-    <?php if($produc->imagen != null): ?>
-    <img src="<?=base_url?>uploads/images/<?=$produc->imagen?>"/>
-    <?php else :?>
-    <img src="<?=base_url?>assets/img/camiseta.png"/>
-    <?php endif; ?>
-
-        <p><?=$produc->descripcion?></p>
-        <p><?=$produc->precio?></p>
-        <a href="#" class="button">comprar</a>
- 
+    <div id="detail-product">
+        <div class="image">
+            <?php if ($produc->imagen != null): ?>
+                <img src="<?= base_url ?>uploads/images/<?= $produc->imagen ?>"/>
+            <?php else : ?>
+                <img src="<?= base_url ?>assets/img/camiseta.png"/>
+            <?php endif; ?>
+        </div>
+        <div class="data" >
+            <p class="descripcion"><?= $produc->descripcion ?></p>
+            <p class="price"><?= $produc->precio ?></p>
+            <a href="#" class="button">comprar</a>
+        </div>
+    </div>
 <?php else : ?>
     <h1>El producto no existe</h1>
 <?php endif; ?>
