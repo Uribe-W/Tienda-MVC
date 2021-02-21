10dfe901f3844c23e613aee8ce281f2fb717a3d4
W.uribe <wilsonuribe16@gmail.com>
2021-02-21 03:42:29 +0100

    back de el carrito de compras

--- controllers/carritoController.php
+++ controllers/carritoController.php
@@ -17,13 +17,23 @@ class carritoController {
         }
 
         if (isset($_SESSION['carrito'])) {
-            
-        } else {
+            $counter = 0;
+            foreach ($_SESSION['carrito'] as $indice => $elemento){
+                if($elemento['id_producto'] == $producto_id){
+                    $_SESSION['carrito'][$indice]['unidades']++;
+                    $counter++;
+                }
+            }           
+        }
+        
+        if(!isset($counter)|| $counter == 0){
             //conseguir producto
             $producto = new Producto;
             $producto->setId($producto_id);
             $producto = $producto->getOne();
-
+           
+            
+            //añadir al carrito
             if (is_object($producto)) {
                 $_SESSION['carrito'][] = array(
                     "id_producto" => $producto->id,
@@ -33,6 +43,7 @@ class carritoController {
                 );
             }
         }
+        
         header('location:'.base_url.'carrito/index');
     }
 
